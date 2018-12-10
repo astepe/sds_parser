bfa00f832b352adb2c414747d86e4bca11f27a51
Aris Stepe <arisstepe@gmail.com>
2018-12-10 13:20:37 -0500

    added chemical name searching and refactored

--- parser.py
+++ parser.py
@@ -3,44 +3,87 @@ import re
 import csv
 from PyPDF2 import PdfFileReader
 
-path = os.getcwd() + "/sds_pdf_files/"
-directory = os.fsencode(path)
-flash_point = re.compile(r"[F|f]lash [P|p]oint[^a-bd-zA-BD-Z]*?([0-9][^C]*?F)", re.DOTALL)
-specific_gravity = re.compile(r"[R|r]elative [D|d]ensity(.){50}", re.DOTALL)
-cas_num = re.compile(r"CAS(.){50}", re.DOTALL)
 
-with open('data.csv', 'w') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(["Filename", "Flash Point", "Specific Gravity", "CAS #"])
-    for file in os.listdir(directory):
-        filename = path + os.fsdecode(file)
-        if filename.endswith(".pdf"):
-            csv_row = [filename.split('/')[-1].split('.')[0]]
-            input_file = PdfFileReader(open(filename, "rb"))
-            text = ''
-            for page_num in range(input_file.getNumPages()):
-                try:
-                    text += input_file.getPage(page_num).extractText()
-                except:
-                    pass
+def write_to_csv():
 
-            text = ''.join(text.split('\n'))
+    sds_directory = os.getcwd() + "/sds_pdf_files/"
 
-            flash = flash_point.search(text)
-            spec = specific_gravity.search(text)
-            cas = cas_num.search(text)
+    with open('data.csv', 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Product Name",
+                         "Flash Point",
+                         "Specific Gravity",
+                         "CAS #"
+                         ])
 
-            if flash:
-                csv_row.append(flash.group(1))
-            else:
-                csv_row.append('None Found')
-            if spec:
-                csv_row.append(spec[0])
-            else:
-                csv_row.append('None Found')
-            if cas:
-                csv_row.append(cas[0])
-            else:
-                csv_row.append('None Found')
+        for file in os.listdir(sds_directory):
+            file_path = sds_directory + file
 
-            writer.writerow(csv_row)
+            print('---------------------------------------------', end='\n')
+            print('file_name: ', file)
+
+            if file_path.endswith(".pdf"):
+
+                raw_text = get_pdf_text(file_path)
+
+                matches = match_fields(raw_text)
+
+                csv_row = [matches['product_name'],
+                           matches['flash_point'],
+                           matches['specific_gravity'],
+                           matches['cas_number']
+                           ]
+
+                writer.writerow(csv_row)
+
+
+def get_pdf_text(file_path):
+
+    text = ''
+
+    with open(file_path, "rb") as _:
+        pdf = PdfFileReader(_, 'rb')
+        for page_num in range(pdf.getNumPages()):
+            try:
+                text += pdf.getPage(page_num).extractText()
+            except:
+                pass
+    return text
+
+
+def match_fields(text):
+
+    product_name = re.compile(r"[P|p]roduct (([N|n]ame)|([D|d]escription)).*?(?P<data>[^ :\n].*?)(P|C)", re.DOTALL)
+    flash_point = re.compile(r"[F|f]lash [P|p]oint[^a-bd-zA-BD-Z]*?(?P<data>[0-9][^C]*?F)", re.DOTALL)
+    specific_gravity = re.compile(r"([R|r]elative [D|d]ensity|[S|s]pecific [G|g]ravity)\s*?(?P<data>[0-9.]+?\s)", re.DOTALL)
+    cas_num = re.compile(r"(?P<data>\d{2,7}\n*?-\n*?\d{2}\n*?-\n*?\d)", re.DOTALL)
+
+    matches = {'product_name': 'Not Found',
+               'flash_point': 'Not Found',
+               'specific_gravity': 'Not Found',
+               'cas_number': 'Not Found',
+               }
+
+    product_match = product_name.search(text)
+    flash_match = flash_point.search(text)
+    spec_match = specific_gravity.search(text)
+    cas_match = cas_num.search(text)
+
+    if product_match:
+        matches['product_name'] = product_match.group('data').replace('\n', '')
+    if flash_match:
+        matches['flash_point'] = flash_match.group('data').replace('\n', '')
+    if spec_match:
+        matches['specific_gravity'] = spec_match.group('data').replace('\n', '')
+    if cas_match:
+        matches['cas_number'] = cas_match.group('data').replace('\n', '')
+
+    for name, match in matches.items():
+        print(name, ' :', match)
+    print('\n')
+
+    return matches
+
+
+if __name__ == '__main__':
+    write_to_csv()
