7efd34dc3a66f1ed0b8399f6d62d3ac1f9082e41
Aris Stepe <arisstepe@gmail.com>
2019-02-22 21:30:47 -0500

    updated static method names

--- sdsparser/parser.py
+++ sdsparser/parser.py
@@ -32,23 +32,6 @@ class SDSParser:
         self.force_ocr = False
         self.debug = debug
 
-    @staticmethod
-    def find_matching_text_file(sds_file_path, sds_text_files):
-        """
-        find txt file with same name as sds file
-        and return the path to the txt file if found
-        """
-        sds_file_name = sds_file_path.split('.')[0].split('/')[-1]
-        for text_file in os.listdir(sds_text_files):
-            if text_file.startswith(sds_file_name):
-                return os.path.join(sds_text_files, text_file)
-        return None
-
-    @staticmethod
-    def get_text_from_file(text_file_path):
-        with open(text_file_path, 'r') as text_file:
-            return text_file.read()
-
     def get_sds_data(self, sds_file_path, extract_method=None):
         """
         retrieve requested sds data
@@ -80,58 +63,6 @@ class SDSParser:
         return sds_data
 
     @staticmethod
-    def add_file_info(sds_data, file_info):
-        sds_data['format'] = file_info['manufacturer']
-        sds_file_path = file_info['sds_file_path']
-        sds_data['filename'] = sds_file_path.split('/')[-1]
-        text_file_path = SDSParser.find_matching_text_file(sds_file_path,
-                                                           Configs.SDS_TEXT_FILES)
-        if text_file_path is not None:
-            _ocr_ran = 'ocr' in text_file_path.split('/')[-1]
-        else:
-            _ocr_ran = file_info['ocr_ran']
-        sds_data['extract method'] = 'ocr' if _ocr_ran else 'text'
-
-        return sds_data
-
-    def define_extract_method(self, extract_method):
-        if extract_method == 'ocr':
-            self.force_ocr = True
-        if extract_method == 'text':
-            self.ocr_override = False
-
-    def reset_state(self):
-        self.ocr_override = True
-        self.ocr_ran = False
-        self.force_ocr = False
-
-    def define_regexes(manufacturer):
-
-        if manufacturer is not None:
-            return SDSParser.compile_regexes(SDSRegexes.SDS_FORMAT_REGEXES[manufacturer])
-        else:
-            return SDSParser.compile_regexes(SDSRegexes.DEFAULT_SDS_FORMAT)
-
-    def search_sds_text(self, sds_text, regexes):
-        """
-        construct a dictionary by iterating over each data request and
-        performing a regular expression match
-        """
-
-        sds_data = {}
-
-        for request_key in self.request_keys:
-
-            if request_key in regexes:
-
-                regex = regexes[request_key]
-                match = SDSParser.find_match(sds_text, regex)
-
-                sds_data[request_key] = match
-
-        return sds_data
-
-    @staticmethod
     def check_empty_matches(sds_data):
         """
         check if data not listed
@@ -142,81 +73,16 @@ class SDSParser:
                 return False
         return True
 
-    @staticmethod
-    def get_match_string(matches):
-        """
-        retrieve matched group string
-        """
+    def reset_state(self):
+        self.ocr_override = True
+        self.ocr_ran = False
+        self.force_ocr = False
 
-        group_matches = 0
-        match_string = ''
-
-        for name, group in matches.groupdict().items():
-            if group is not None:
-
-                group = group.replace('\n', '').strip()
-
-                if group_matches > 0:
-                    match_string += ', ' + group
-                else:
-                    match_string += group
-
-                group_matches += 1
-
-        if match_string:
-            return match_string
-        else:
-            return 'No data available'
-
-    @staticmethod
-    def find_match(sds_text, regex):
-        """
-        perform a regular expression match and return matched data
-        """
-
-        matches = regex.search(sds_text)
-
-        if matches is not None:
-
-            return SDSParser.get_match_string(matches)
-
-        else:
-
-            return 'Data not listed'
-
-    @staticmethod
-    def get_manufacturer(sds_text):
-        """
-        define set of regular expressions to be used for data matching by searching
-        for the manufacturer name within the sds text
-        """
-
-        for manufacturer, regexes in SDSRegexes.SDS_FORMAT_REGEXES.items():
-
-            regex = re.compile(*regexes['manufacturer'])
-
-            match = regex.search(sds_text)
-
-            if match:
-                print(f'using {manufacturer} format...')
-                return manufacturer
-
-        print('using default sds format...')
-        return None
-
-    @staticmethod
-    def compile_regexes(regexes):
-        """
-        return a dictionary of compiled regular expressions
-        """
-
-        compiled_regexes = {}
-
-        for name, regex in regexes.items():
-
-            compiled_regexes[name] = re.compile(*regex)
-
-        return compiled_regexes
+    def define_extract_method(self, extract_method):
+        if extract_method == 'ocr':
+            self.force_ocr = True
+        if extract_method == 'text':
+            self.ocr_override = False
 
     def get_sds_text(self, sds_file_path):
         """
@@ -243,40 +109,21 @@ class SDSParser:
         return sds_text
 
     @staticmethod
-    def get_sds_pdf_text(sds_file_path):
+    def find_matching_text_file(sds_file_path, sds_text_files):
         """
-        extract text directly from pdf file
+        find txt file with same name as sds file
+        and return the path to the txt file if found
         """
+        sds_file_name = sds_file_path.split('.')[0].split('/')[-1]
+        for text_file in os.listdir(sds_text_files):
+            if text_file.startswith(sds_file_name):
+                return os.path.join(sds_text_files, text_file)
+        return None
 
-        text = ''
-        resource_manager = PDFResourceManager()
-        fake_file_handle = io.StringIO()
-        converter = TextConverter(resource_manager, fake_file_handle)
-        page_interpreter = PDFPageInterpreter(resource_manager, converter)
-
-        try:
-            with open(sds_file_path, 'rb') as fh:
-                for page in PDFPage.get_pages(fh,
-                                              caching=True,
-                                              check_extractable=True):
-                    page_interpreter.process_page(page)
-
-                text = fake_file_handle.getvalue()
-
-        except PDFTextExtractionNotAllowed:
-            pass
-        # close open handles
-        converter.close()
-        fake_file_handle.close()
-
-        return text
-
-    def get_sorted_dir_list(path):
-
-        dir_list = os.listdir(path)
-        regex = re.compile(r"[\d]*(?=\.jpg)")
-        dir_list.sort(key=lambda x: regex.search(x)[0])
-        return dir_list
+    @staticmethod
+    def get_text_from_file(text_file_path):
+        with open(text_file_path, 'r') as text_file:
+            return text_file.read()
 
     @staticmethod
     def get_sds_image_text(sds_file_path):
@@ -308,6 +155,160 @@ class SDSParser:
             progress_bar.update(100)
             print()
             return sds_image_text
+
+    @staticmethod
+    def get_sorted_dir_list(path):
+
+        dir_list = os.listdir(path)
+        regex = re.compile(r"[\d]*(?=\.jpg)")
+        dir_list.sort(key=lambda x: regex.search(x)[0])
+        return dir_list
+
+    @staticmethod
+    def get_sds_pdf_text(sds_file_path):
+        """
+        extract text directly from pdf file
+        """
+
+        text = ''
+        resource_manager = PDFResourceManager()
+        fake_file_handle = io.StringIO()
+        converter = TextConverter(resource_manager, fake_file_handle)
+        page_interpreter = PDFPageInterpreter(resource_manager, converter)
+
+        try:
+            with open(sds_file_path, 'rb') as fh:
+                for page in PDFPage.get_pages(fh,
+                                              caching=True,
+                                              check_extractable=True):
+                    page_interpreter.process_page(page)
+
+                text = fake_file_handle.getvalue()
+
+        except PDFTextExtractionNotAllowed:
+            pass
+        # close open handles
+        converter.close()
+        fake_file_handle.close()
+
+        return text
+
+    @staticmethod
+    def get_manufacturer(sds_text):
+        """
+        define set of regular expressions to be used for data matching by searching
+        for the manufacturer name within the sds text
+        """
+
+        for manufacturer, regexes in SDSRegexes.SDS_FORMAT_REGEXES.items():
+
+            regex = re.compile(*regexes['manufacturer'])
+
+            match = regex.search(sds_text)
+
+            if match:
+                print(f'using {manufacturer} format...')
+                return manufacturer
+
+        print('using default sds format...')
+        return None
+
+    def define_regexes(manufacturer):
+
+        if manufacturer is not None:
+            return SDSParser.compile_regexes(SDSRegexes.SDS_FORMAT_REGEXES[manufacturer])
+        else:
+            return SDSParser.compile_regexes(SDSRegexes.DEFAULT_SDS_FORMAT)
+
+    @staticmethod
+    def compile_regexes(regexes):
+        """
+        return a dictionary of compiled regular expressions
+        """
+
+        compiled_regexes = {}
+
+        for name, regex in regexes.items():
+
+            compiled_regexes[name] = re.compile(*regex)
+
+        return compiled_regexes
+
+    def search_sds_text(self, sds_text, regexes):
+        """
+        construct a dictionary by iterating over each data request and
+        performing a regular expression match
+        """
+
+        sds_data = {}
+
+        for request_key in self.request_keys:
+
+            if request_key in regexes:
+
+                regex = regexes[request_key]
+                match = SDSParser.find_match(sds_text, regex)
+
+                sds_data[request_key] = match
+
+        return sds_data
+
+    @staticmethod
+    def find_match(sds_text, regex):
+        """
+        perform a regular expression match and return matched data
+        """
+
+        matches = regex.search(sds_text)
+
+        if matches is not None:
+
+            return SDSParser.get_match_string(matches)
+
+        else:
+
+            return 'Data not listed'
+
+    @staticmethod
+    def get_match_string(matches):
+        """
+        retrieve matched group string
+        """
+
+        group_matches = 0
+        match_string = ''
+
+        for name, group in matches.groupdict().items():
+            if group is not None:
+
+                group = group.replace('\n', '').strip()
+
+                if group_matches > 0:
+                    match_string += ', ' + group
+                else:
+                    match_string += group
+
+                group_matches += 1
+
+        if match_string:
+            return match_string
+        else:
+            return 'No data available'
+
+    @staticmethod
+    def add_file_info(sds_data, file_info):
+        sds_data['format'] = file_info['manufacturer']
+        sds_file_path = file_info['sds_file_path']
+        sds_data['filename'] = sds_file_path.split('/')[-1]
+        text_file_path = SDSParser.find_matching_text_file(sds_file_path,
+                                                           Configs.SDS_TEXT_FILES)
+        if text_file_path is not None:
+            _ocr_ran = 'ocr' in text_file_path.split('/')[-1]
+        else:
+            _ocr_ran = file_info['ocr_ran']
+        sds_data['extract method'] = 'ocr' if _ocr_ran else 'text'
+
+        return sds_data
 
 
 if __name__ == '__main__':
